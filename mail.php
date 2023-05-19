81f593cd9d03d3c37c1a168d7c0548c77a67a1af
Cast3c <ricardo.castellarp@gmail.com>
2023-05-19 15:54:21 -0500

    final version added

--- mail.php
+++ mail.php
@@ -1,6 +1,33 @@
 <?php
+<<<<<<< HEAD
 
     $nombre = $_POST['nombreEmpresa'];
 
     var_dump($nombre);
     
+=======
+$nombre = $_POST['nombreEmpresa'];
+$telefono = $_POST['telefonoContacto'];
+$ciudad = $_POST['ciudadContacto'];
+$mail = $_POST['mailContacto'];
+
+$body = <<<HTML
+    <h2>Contacto desde Pagina Web</h2>
+    <p>De: $nombre / $mail </p>
+    <h3>Mensaje: </h3>
+    <p>Quiero ser contactad@ y recibir mas informacion:<br>
+        $telefono<br>
+        $ciudad<br>
+        $mail<br>
+    </p>
+HTML;
+
+$headers ="MIME-Version: 1.0 \r\n";
+$headers.="Content-type: text/html; charset=utf-8 \r\n";
+//REMITENTE (NOMBRE/APELLIDO)
+//ASUNTO
+//CUERPO
+mail('sanityking@gmail.com', 'Email pagina web', $body, $headers);
+
+header("Location:https://sanityking.com/#contacto");
+>>>>>>> d211e769da4d84b3bc79ef9b8b4944b9daf6acc1
