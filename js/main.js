2699ed4cb4b437f21e47963052bf6fca6d499295
Cast3c <ricardo.castellarp@gmail.com>
2023-06-21 11:36:38 -0500

    modal2 init added

--- js/main.js
+++ js/main.js
@@ -4,17 +4,20 @@ document.addEventListener('DOMContentLoaded', function() {
     //=========MODALES=========//
     var modalBtn1 = document.querySelector('.modal-btn1');
     var modalBtn2 = document.querySelector('.modal-btn2');
+    var modalBtn3 = document.querySelector('.modal-btn3');
     var modalClose1 = document.querySelector('.modal-close1');
     var modalClose2 = document.querySelector('.modal-close2');
-    var modalDesinfeccion = document.querySelector('.modal-desinfeccion');
+    var modalClose3 = document.querySelector('.modal-close3');
+    var modalTanques = document.querySelector('.modal-tanques');
     var modalFumigacion = document.querySelector('.modal-fumigacion');
+    var modalDesinfeccion = document.querySelector('.modal-desinfeccion');
     var overlay = document.querySelector('.overlay');
     
     var body = document.querySelector('body');
   
     modalBtn1.addEventListener('click', function(event) {
       event.preventDefault();
-      modalDesinfeccion.style.display = 'flex';
+      modalTanques.style.display = 'flex';
       overlay.style.display = 'block';
       body.style.overflow = 'hidden';
     });
@@ -25,10 +28,17 @@ document.addEventListener('DOMContentLoaded', function() {
       overlay.style.display = 'block';
       body.style.overflow = 'hidden';
     })
-  
+    
+    modalBtn3.addEventListener('click', function(event){
+      event.preventDefault();
+      modalDesinfeccion.style.display = 'flex';
+      overlay.style.display = 'block';
+      body.style.overflow = 'hidden';
+    })
+
     modalClose1.addEventListener('click', function(event) {
       event.preventDefault();
-      modalDesinfeccion.style.display = 'none';
+      modalTanques.style.display = 'none';
       overlay.style.display = 'none';
       body.style.overflow = 'auto';
     });
@@ -36,6 +46,13 @@ document.addEventListener('DOMContentLoaded', function() {
     modalClose2.addEventListener('click', function(event){
       event.preventDefault();
       modalFumigacion.style.display = 'none';
+      overlay.style.display = 'none';
+      body.style.overflow = 'auto';
+    })
+
+    modalClose3.addEventListener('click', function(event){
+      event.preventDefault();
+      modalDesinfeccion.style.display = 'none';
       overlay.style.display = 'none';
       body.style.overflow = 'auto';
     })
